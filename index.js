2597c1ab70754c2d0255f22f04c80b4a4640a467
JayKopinski <jason.kopinski@shopify.com>
2018-04-03 20:15:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -23,45 +23,7 @@ function cryptoCron() {
 
   var myCron = schedule.scheduleJob(timer, function() {
     console.log('Running Cron');
-    // For each product ID in the productID array that gets returned, get data
-    for (var key in productDict) {
-      getProductPrice(productDict[key], function(data) {
-        var p = JSON.parse(data);
-        var productVariants = p.product.variants;
-
-        // First check that the price is correct
-        var split = productVariants[0].title.split(" ");
-        var qty = split[0];
-        var coinCode = split[1];
-
-        var q = qty * (1 / qty); // Make sure we're looking at one of the coin to check price
-
-        // Check if the crypto price matches
-        getCryptoPrice(coinCodes[coinCode], function(data){
-          var coinMarketPrice = data[0].price_usd;
-          prices[coinCode] = data[0].price_usd;
-          // console.log(coinMarketPrice);
-          // Check if price !== Coin Market Price. * qty is added so that if we're
-          // checking a variant that is 0.5 btc or 100 btc it will still work.
-          if (((productVariants[0].price * qty) / profitMultiplier)  !== (coinMarketPrice * qty)) {
-            // For each of the variants in the product get the code and the price.
-            productVariants.forEach(function(v) {
-              var split = v.title.split(" "); // Seperate the code by the space (" ")
-              var qty = split[0]; // btc
-              var price = (coinMarketPrice * qty) * profitMultiplier;
-              // console.log(coinMarketPrice * qty , v.price);
-              updateVariantPrice(v.id, price);
-              // console.log("Qty: ", qty," Code: ", coinCode, q);
-            });
-          } else {
-            console.log("Prices Match!");
-          }
-        });
-
-      });
-    }
-  });
-}
+   
 
 function getAllCryptoPrice(callback) {
   // FUNCTION TO GET CRYPTO PRICE.
